5259657c425218e18698d231726d1f6d07417cd6
dhaniemubarak <dhaniemubarak@gmail.com>
2022-04-06 15:38:58 +0700

    Initial commit

---
+++ src/main/java/id/holigo/services/holigocouponservice/HoligoCouponServiceApplication.java
@@ -0,0 +1,13 @@
+package id.holigo.services.holigocouponservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoligoCouponServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoligoCouponServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/holigo/services/holigocouponservice/HoligoCouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.holigo.services.holigocouponservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoligoCouponServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
