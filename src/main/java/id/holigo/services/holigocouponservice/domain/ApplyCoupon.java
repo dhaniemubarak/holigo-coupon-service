fa3df50ac01c6723797a34533962d64b9dd93571
Mochamad Ramdhanie Mubarak <dhaniemubarak@gmail.com>
2022-09-01 02:35:27 +0700

    Create apply coupon 1

---
+++ src/main/java/id/holigo/services/holigocouponservice/domain/ApplyCoupon.java
@@ -0,0 +1,56 @@
+package id.holigo.services.holigocouponservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApplyCoupon {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    @Type(type = "org.hibernate.type.UUIDCharType")
+    private UUID id;
+
+    private String couponCode;
+
+    @ManyToOne
+    private Coupon coupon;
+
+
+    private Long userId;
+
+    private UUID transactionId;
+
+    private Boolean isValid;
+
+    private Boolean isFreeAdmin;
+
+    private Boolean isFreeServiceFee;
+
+    private BigDecimal discountAmount;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+
+
+}
